a06f60984ca56c20745a265d36c0d810fb9b0182
AKK-soft-dev <aungkokosoftdev@gmail.com>
2023-10-20 20:25:30 +0630

    update(home): now we can see tasks with specific ranges

--- src/components/reusable/SubCategoryItem/types.d.ts
+++
@@ -1,7 +0,0 @@
-export interface CategoryType {
-  id: string | number;
-  name: string;
-  description: string;
-  createdAt: string;
-  parentId?: string;
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,68 @@
-import Next30Days from "../components/Home/Next30Days/Next30Days";
-import Next7Days from "../components/Home/Next7Days/Next7Days";
-import Today from "../components/Home/Today/Today";
-import Tomorrow from "../components/Home/Tomorrow/Tomorrow";
+import { addDays, isAfter, isBefore, isToday, isTomorrow } from "date-fns";
+import { selectAllTodo } from "../redux/features/todo/todoSlice";
+import { useAppSelector } from "../redux/hooks";
+import { TodoType } from "../redux/features/featureTypes";
+import TodoListWithSpecificDays from "../components/TodoListWithSpecificDays/TodoListWithSpecificDays";
+import { formatDateToStr } from "../utils/formatDate";
 
 const HomePage = () => {
+  const today = new Date();
+  const tomorrow = addDays(today, 1);
+  const next7Days = addDays(tomorrow, 7);
+  const next30Days = addDays(next7Days, 30);
+
+  const todayTodo: TodoType[] = [];
+  const tomorrowTodo: TodoType[] = [];
+  const next7DaysTodo: TodoType[] = [];
+  const next30DaysTodo: TodoType[] = [];
+
+  const todoList = useAppSelector(selectAllTodo);
+
+  todoList.forEach((todo) => {
+    const dueDate = new Date(todo.dueDate);
+    if (isToday(dueDate)) {
+      todayTodo.push(todo);
+    } else if (isTomorrow(dueDate)) {
+      tomorrowTodo.push(todo);
+    } else if (isAfter(dueDate, tomorrow) && isBefore(dueDate, next7Days)) {
+      next7DaysTodo.push(todo);
+    } else if (isAfter(dueDate, next7Days) && isBefore(dueDate, next30Days)) {
+      next30DaysTodo.push(todo);
+    }
+  });
+
   return (
     <div className="flex flex-col">
-      <Today />
-      <Tomorrow />
-      <Next7Days />
-      <Next30Days />
+      <TodoListWithSpecificDays
+        title="Today"
+        dateStr={formatDateToStr(today, true)}
+        messageOnEmptyList="You don't have any tasks for today"
+        todoList={todayTodo}
+      />
+      <TodoListWithSpecificDays
+        title="Tomorrow"
+        dateStr={formatDateToStr(tomorrow, true)}
+        messageOnEmptyList="You don't have any tasks for tomorrow"
+        todoList={tomorrowTodo}
+      />
+      <TodoListWithSpecificDays
+        title="Next 7 Days"
+        dateStr={`${formatDateToStr(
+          addDays(tomorrow, 1),
+          true
+        )} - ${formatDateToStr(next7Days, true)}`}
+        messageOnEmptyList="You don't have any tasks for the next 7 days"
+        todoList={next7DaysTodo}
+      />
+      <TodoListWithSpecificDays
+        title="Next 30 Days"
+        dateStr={`${formatDateToStr(
+          addDays(next7Days, 1),
+          true
+        )} - ${formatDateToStr(next30Days, true)}`}
+        messageOnEmptyList="You don't have any tasks for the next 30 days"
+        todoList={next30DaysTodo}
+      />
     </div>
   );
 };
